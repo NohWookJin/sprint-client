6f790a832073e172cbb6f1a16e9270dab7a2d42f
NohWookJin <jinn98@kakao.com>
2024-03-26 07:03:21 +0900

    Markup(#4): Markup Mainpage and Link with Mock data

---
+++ src/Components/Analysis/Analysis.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ActivitiesResponse, generateDummyData } from "../../API/getAnalysis";
+import AnalysisItem from "./AnalysisItem";
+
+const Analysis: React.FC = () => {
+  const jsonData: ActivitiesResponse = generateDummyData();
+  const activities = Object.values(jsonData.response).flat();
+
+  return (
+    <section className="pb-8 px-4 mb-[20px]">
+      <div className="font-bold">
+        <span className="text-[23px]">스프린터님의 루틴 기록이에요.</span>
+      </div>
+      <div className="flex flex-col gap-5 mt-5">
+        {activities.map((activity, index) => (
+          <AnalysisItem
+            key={index}
+            calendar={activity.calendar[0]}
+            name={activity.name}
+            start_with={activity.start_with}
+            continuity={activity.continuity}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Analysis;
