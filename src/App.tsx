eb30c7db327944f3dcd568b6f7f3a60b1afcd4c2
NohWookJin <jinn98@kakao.com>
2024-05-29 13:11:40 +0900

    Refactor: Delete react-query tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import { RecoilRoot } from "recoil";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter } from "react-router-dom";
 import MainRouter from "./Router/MainRouter";
 
-const queryClient = new QueryClient();
-
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <BrowserRouter>
-          <MainRouter />
-        </BrowserRouter>
-      </RecoilRoot>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <BrowserRouter>
+        <MainRouter />
+      </BrowserRouter>
+    </RecoilRoot>
   );
 };
 export default App;
