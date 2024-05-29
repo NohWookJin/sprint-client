3aa02ad37959f14593fdfbeaba0b5b4b19494dc8
NohWookJin <jinn98@kakao.com>
2024-05-29 12:34:58 +0900

    Feat: Mobile User Blur

--- src/Router/RouterLayout.tsx
+++ src/Router/RouterLayout.tsx
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Components/Common/Header";
+import ScreenMessage from "../Components/Common/ScreenMessage";
 import "../index.css";
 
 const MainLayout = () => {
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth <= 640);
+      if (window.innerWidth <= 640) {
+        document.body.style.overflow = "hidden";
+      } else {
+        document.body.style.overflow = "auto";
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="mx-auto min-h-dvh min-w-[360px] max-w-[640px]">
+    <div className="relative mx-auto min-h-screen min-w-[360px] max-w-[640px]">
       <Header />
-      <div className="h-full min-w-[640px]">
+      {isMobileView && <ScreenMessage />}
+      <div className={`h-full ${isMobileView ? "blur-sm" : ""}`}>
         <Outlet />
       </div>
     </div>
